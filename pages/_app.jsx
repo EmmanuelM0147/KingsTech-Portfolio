efa0e5ddebe4de046b21d263393713bb4c4f2086
Emmanuel Okeowo <okeowoemmanuelm@gmail.com>
2024-10-24 23:08:49 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,47 +1,42 @@
-// Core packages
 import { Analytics } from '@vercel/analytics/react';
 import { LazyMotion, domAnimation } from "framer-motion"
-
-// Utils
+import Head from 'next/head'; // Import the Head component
 import SetGridGap from '../components/utils/set.grid.util'
-
-// Structure
 import Layout from '../components/layout/layout'
-
-// CSS reset (https://github.com/elad2412/the-new-css-reset.git)
 import "../node_modules/the-new-css-reset/css/reset.css"
-
-// Fontsource local font import (https://github.com/fontsource/fontsource)
 import "@fontsource/fira-code/400.css"
 import "@fontsource/fira-code/600.css"
 import "@fontsource/inter/400.css"
 import "@fontsource/inter/700.css"
 import "@fontsource/inter/800.css"
-
-// Devicon import (https://github.com/devicons/devicon)
 import '../node_modules/devicon/devicon.min.css'
-
-// Global css
 import '../styles/css/variables.css'
 import '../styles/css/global.css'
 
-/**
- * _app.jsx
- *
- * @param {?} Component
- * @param {?} pageProps
- * @returns
- */
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<>
-		<LazyMotion features={domAnimation}>
-			<Layout>
-				<Component {...pageProps} />
-				<SetGridGap />
-				<Analytics />
-			</Layout>
-		</LazyMotion>
+			<Head>
+				<script
+					data-name="BMC-Widget"
+					data-cfasync="false"
+					src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+					data-id="emmanuelokeowo"
+					data-description="Support me on Buy me a coffee!"
+					data-message=""
+					data-color="#87CEEB"
+					data-position="Right"
+					data-x_margin="18"
+					data-y_margin="18"
+				></script>
+			</Head>
+			<LazyMotion features={domAnimation}>
+				<Layout>
+					<Component {...pageProps} />
+					<SetGridGap />
+					<Analytics />
+				</Layout>
+			</LazyMotion>
 		</>
 	)
 }
