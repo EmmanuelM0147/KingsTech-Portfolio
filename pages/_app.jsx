64f35db4834c501395e4f2896d57f28766b2029c
Emmanuel Okeowo <okeowoemmanuelm@gmail.com>
2024-10-25 07:23:46 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import { Analytics } from '@vercel/analytics/react';
 import { LazyMotion, domAnimation } from "framer-motion"
-import Head from 'next/head'; // Import the Head component
+import Head from 'next/head';
+import Script from 'next/script'; // Import next/script for loading scripts
 import SetGridGap from '../components/utils/set.grid.util'
 import Layout from '../components/layout/layout'
 import "../node_modules/the-new-css-reset/css/reset.css"
@@ -14,29 +15,31 @@ import '../styles/css/variables.css'
 import '../styles/css/global.css'
 
 export default function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Head>
-				<script
-					data-name="BMC-Widget"
-					data-cfasync="false"
-					src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-					data-id="emmanuelokeowo"
-					data-description="Support me on Buy me a coffee!"
-					data-message=""
-					data-color="#87CEEB"
-					data-position="Right"
-					data-x_margin="18"
-					data-y_margin="18"
-				></script>
-			</Head>
-			<LazyMotion features={domAnimation}>
-				<Layout>
-					<Component {...pageProps} />
-					<SetGridGap />
-					<Analytics />
-				</Layout>
-			</LazyMotion>
-		</>
-	)
+    return (
+        <>
+            <Head>
+                <title>My App</title>
+            </Head>
+            <Script
+                src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+                data-name="BMC-Widget"
+                data-cfasync="false"
+                data-id="emmanuelokeowo"
+                data-description="Support me on Buy me a coffee!"
+                data-message=""
+                data-color="#87CEEB"
+                data-position="Right"
+                data-x_margin="18"
+                data-y_margin="18"
+                strategy="lazyOnload" // Load script lazily
+            />
+            <LazyMotion features={domAnimation}>
+                <Layout>
+                    <Component {...pageProps} />
+                    <SetGridGap />
+                    <Analytics />
+                </Layout>
+            </LazyMotion>
+        </>
+    )
 }
